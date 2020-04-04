6ec91fdb0ecf35ae12bfaac2528631ac1edd273f
Fetzle <lui.seyboldt@gmail.com>
2020-04-04 16:32:54 +0200

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod audio_player;
+
 fn main() {
+    let mut input = String::new();
     println!("Hello, world!");
+    let sink = audio_player::play_music();
+    std::io::stdin().read_line(&mut input);
 }
