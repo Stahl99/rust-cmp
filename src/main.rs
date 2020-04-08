82a37f58d061c83faf33cb18e558bde7ca040ade
Sven Stahl <50907202+Stahl99@users.noreply.github.com>
2020-04-09 00:25:44 +0200

    Implements simple list

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,7 @@ use std::io;
 
 fn main() {
     let mut terminal = terminal::init_terminal();
+
     terminal::draw_terminal(&mut terminal);
+
 }
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,7 +1,8 @@
 use std::io::{stdin, stdout, Write, Error};
 use tui::{backend::CrosstermBackend, Terminal};
-use tui::widgets::{Widget, Block, Borders};
+use tui::widgets::{Widget, Block, Borders, List, Text, ListState};
 use tui::layout::{Layout, Constraint, Direction};
+use tui::style::{Color, Modifier, Style};
 use crossterm::{
     event::{self, Event as CEvent, KeyCode},
     execute,
@@ -9,15 +10,18 @@ use crossterm::{
 };
 
 pub fn init_terminal() -> Terminal<CrosstermBackend<std::io::Stdout>> {
-    let mut stdout = stdout()/*.into_raw_mode().unwrap()*/;
+    let mut stdout = stdout()/*.into_raw_mode()*/;
     let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend);
+    let mut terminal = Terminal::new(backend).unwrap();
 
-    return terminal.unwrap();
+    terminal.hide_cursor();
+    terminal.clear();
+
+    return terminal;
 }
 
 pub fn draw_terminal(terminal : &mut Terminal<CrosstermBackend<std::io::Stdout>>) -> () {
-    terminal.draw(|mut f| {
+    /*terminal.draw(|mut f| {
         let chunks = Layout::default()
             .direction(Direction::Horizontal)
             .margin(1)
@@ -30,12 +34,45 @@ pub fn draw_terminal(terminal : &mut Terminal<CrosstermBackend<std::io::Stdout>>
             )
             .split(f.size());
         Block::default()
-             .title("Block")
+             .title(&format!("Block: {}", number.to_string()).to_owned())
              .borders(Borders::ALL)
              .render(&mut f, chunks[0]);
         Block::default()
-             .title("Block 2")
+             .title("Block 2: ")
              .borders(Borders::ALL)
              .render(&mut f, chunks[2]);
-    });
+    });*/
+
+    terminal.draw(|mut f| {
+        let chunks = Layout::default()
+            .direction(Direction::Horizontal)
+            .margin(1)
+            .constraints(
+                [
+                    Constraint::Percentage(10),
+                    Constraint::Percentage(80),
+                    Constraint::Percentage(10)
+                ].as_ref()
+            )
+            .split(f.size());
+        
+        Block::default()
+            .title(&format!("Block 1"))
+            .borders(Borders::ALL)
+            .render(&mut f, chunks[0]);
+        Block::default()
+            .title("Block 2: ")
+            .borders(Borders::ALL)
+            .render(&mut f, chunks[2]);
+
+        let style = Style::default().fg(Color::White).bg(Color::Blue);
+
+        let mut items = ["Item 1", "Item 2", "Item 3"].iter().map(|i| Text::raw(*i));
+        let mut items = List::new(items)
+                .block(Block::default().borders(Borders::ALL).title("List"))
+                .style(style)
+                /*.highlight_style(style.fg(Color::LightGreen).modifier(Modifier::BOLD))*/
+                /*.highlight_symbol(">")*/;
+            f.render(&mut items, chunks[0]);
+        });
 }
