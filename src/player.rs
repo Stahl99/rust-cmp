9578a8184dfa1137f28fc3071e3c30c4b7b0dfb5
Fetzle <lui.seyboldt@gmail.com>
2020-05-08 15:41:44 +0200

    reverted to original version

--- src/player.rs
+++ src/player.rs
@@ -150,18 +150,18 @@ impl Player {
 
     // get all song objects in db 
     pub fn get_all_songs(&mut self, ) -> Vec<Song> {
-        //let songs = self.client.search(&Query::new(), (0, 0)).unwrap();
-        let songs = self.client.listall().unwrap();
+        let songs = self.client.search(&Query::new(), (0, 0)).unwrap();
 
         songs
     }
 
     // get all song titles in db
     pub fn get_all_song_titles(&mut self) -> Vec<String> {
-        //let songs = self.client.search(&Query::new(), (1, 100)).unwrap();
-        let songs = self.client.listall().unwrap();
+        let songs = self.client.search(&Query::new(), (0, 1));
+        let songs_ = songs.unwrap();
+        //let songs = self.client.listall().unwrap();
         let mut song_titles: Vec<String> = Vec::new();
-        for song in songs {
+        for song in songs_ {
             song_titles.push(song.title.unwrap());
         }
 
